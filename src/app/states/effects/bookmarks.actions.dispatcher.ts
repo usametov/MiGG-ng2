86b44c29cc82fe575be18e9ce1ac7c851b8eda33
usametov <usametov@gmail.com>
2017-09-28 14:50:12 -0400

    add comments

--- src/app/states/effects/bookmarks.actions.dispatcher.ts
+++ src/app/states/effects/bookmarks.actions.dispatcher.ts
@@ -2,6 +2,10 @@ import { ActionTypes } from "../actions/bookmarks";
 
 //this is just an action router
 //it will grow and that's totally fine, because it easy to test
+//making use of code-is-data approach
+//the benefit of this approach is that 
+//now we less dependent of bookmarks.effects class
+//this makes it easy to replace it when better alternative emerges 
 export const bookmarksActionDispatcher: { [action: string] : string } = {
   [ActionTypes.REQUEST_BOOKMARKS_BY_TAG_BUNDLE] : "getBookmarksByTagBundle"
 }
