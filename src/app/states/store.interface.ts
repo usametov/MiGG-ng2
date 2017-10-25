32a330a38223b3b0e4ac50302d849f5cc25d890f
usametov <usametov@gmail.com>
2017-10-25 11:40:55 -0400

    clean up

--- src/app/states/store.interface.ts
+++ src/app/states/store.interface.ts
@@ -1,5 +1,4 @@
 import { Observable } from 'rxjs/Observable';
-//import { Action as BaseAction} from "@ngrx/store";
 import { Either } from 'tsmonad';
 import { ServerError } from "app/models/server-error";
 import { Bookmark } from "app/models/bookmark";
@@ -7,7 +6,3 @@ import { Bookmark } from "app/models/bookmark";
 export interface StoreInterface<T> {
   getState(): Observable<T>;
 }
-
-// export interface Action<T> extends BaseAction {
-//   payload?:{ data: T};
-// }
